b0ce8ff355a94b4b7fb2df4d9a4c10d51a8e21b1
Sagar More <146955609+5ifar@users.noreply.github.com>
2024-07-21 17:51:22 +0530

    Deleted Codes/Ad-Hoc Insights/Queries/Insight Query 7.sql

--- Codes/Ad-Hoc Insights/Queries/Insight Query 7.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	CONCAT(MONTHNAME(month), ' (', YEAR(month), ')') AS month,
-	fiscal_year,
-	SUM(gross_sales) AS gross_sales_amount
-FROM gross_sales
-WHERE customer = "Atliq Exclusive"
-GROUP BY month, fiscal_year
-ORDER BY gross_sales_amount DESC;
